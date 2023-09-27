bfd959b8301d99134d13d36e4f3c1c78fa27f9c4
tah01 <tahirkadgaonkar01@gmail.com>
2023-09-27 21:19:43 +0530

    removed unwated code

--- src/components/Expenses/EspenseItem.js
+++ src/components/Expenses/EspenseItem.js
@@ -5,17 +5,12 @@ import ExpenseDate from "./ExpenseDate";
 function ExpenseItem(props) {
   const [title, setTitle] = useState(props.title);
 
-  const clickHandler = () => {
-    setTitle("Updated!!");
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
-        <button onClick={clickHandler}>click</button>
       </div>
     </Card>
   );
