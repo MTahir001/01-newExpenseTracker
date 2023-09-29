f82452057b67c2248a582ef840e2088a2afbaaeb
tah01 <tahirkadgaonkar01@gmail.com>
2023-09-29 16:06:24 +0530

    Body of Chart section complted logic remains

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,16 @@
+import "./Chart.css";
+import ChartBar from "./ChartBar";
+const Chart = (props) => {
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.label}
+          label={dataPoint.label}
+          value={dataPoint.value}
+          max={null}
+        />
+      ))}
+    </div>
+  );
+};
